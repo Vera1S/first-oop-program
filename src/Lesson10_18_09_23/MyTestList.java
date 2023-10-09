d0cc41bcfc83497998189b026ad7858c6ba23bff
Vera <inginer719@gmail.ru>
2023-10-08 21:16:29 -0400

    Готово

--- src/Lesson10_18_09_23/MyTestList.java
+++ src/Lesson10_18_09_23/MyTestList.java
@@ -6,6 +6,8 @@ import java.util.Iterator;
 public class MyTestList {
         public static void main(String[] args) {
         ArrayList<Integer> integers = new ArrayList<>();
+        ArrayList<Integer> integersSecond = new ArrayList<>();
+
         MyTestList a = new MyTestList();
         integers.add(2);
         integers.add(5);
@@ -16,10 +18,30 @@ public class MyTestList {
         for (int i = 0; i < integers.size(); i++){
             System.out.println(integers.get(i));
         }
+        for (int i = 0; i < integers.size(); i++) {
+            int integer = integers.get(i);
+            System.out.println("Значение " + integer);
+            if (integer < 4){
+                System.out.println("Да, это число меньше 4");
+                int index = integers.indexOf(integer);
+                integers.remove(index);
+            }
+
+        }
+            System.out.println(integers);
+
 
         Iterator<Integer> iterator = integers.iterator();
 
-        System.out.println(iterator.hasNext());
-        System.out.println(iterator);
+//        System.out.println(iterator.hasNext());
+//        System.out.println(iterator);
+//            System.out.println(iterator.next());
+//            System.out.println(iterator.hasNext());
+//            System.out.println(iterator);
+//            System.out.println(iterator.next());
+
+            //while (iterator.hasNext()){
+            //    System.out.println(integers.get(integers));
+            //}
     }
 }
