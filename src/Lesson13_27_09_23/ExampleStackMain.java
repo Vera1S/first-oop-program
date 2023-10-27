20027e815396df6b9c4823120e978d808e105f09
Vera <inginer719@gmail.ru>
2023-10-27 11:02:02 -0400

    Готово

--- src/Lesson13_27_09_23/ExampleStackMain.java
+++ src/Lesson13_27_09_23/ExampleStackMain.java
@@ -116,6 +116,32 @@ public class ExampleStackMain {
 //
 //        }
 //    }
+        //Вариант 4 к задаче
+
+        // public static void main(String[] args) {
+        //        System.out.println("Hello world!");
+        //        decode("123456789");
+        //    }
+        //
+        //    public static void decode(String code) {
+        //        if (code == null) {
+        //            return;
+        //        }
+        //
+        //        Stack<Character> stack = new Stack<>();
+        //        for (int i = 0; i < code.length(); i++) {
+        //            char symbol = code.charAt(i);
+        //            stack.push(symbol);
+        //            if (stack.size() == 5 || i == code.length() - 1) {
+        //                while (!stack.isEmpty()) {
+        //                    System.out.println(stack.pop());
+        //                }
+        //            }
+        //        }
+        //
+        //        System.out.println(stack);
+        //
+        //    }
 
     }
 }
