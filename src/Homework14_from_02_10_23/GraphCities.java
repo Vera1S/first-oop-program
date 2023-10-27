b848785257195dacebdb9db92aa75bc51634d9d6
Vera <inginer719@gmail.ru>
2023-10-26 20:25:18 -0400

    Готово

---
+++ src/Homework14_from_02_10_23/GraphCities.java
@@ -0,0 +1,57 @@
+package Homework14_from_02_10_23;
+// Необходимо реализовать на джава такой граф - https://kpolyakov.spb.ru/school/test10bu/4_files/numway2.gif
+//
+//Задание со звездочкой (не сложное, просто дополнительное) Решите на бумаге задачу:
+//http://waksoft.susu.ru/wp-content/uploads/2021/10/graph-schame.jpg
+//Найдите самый короткий путь из Рейкявика в Афины
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GraphCities {
+    private String name;
+    private List<String> city;
+
+    public GraphCities(String name) {
+        this.name = name;
+        this.city = new ArrayList<>();
+    }
+    public void add (String cities){
+        city.add(cities);
+    }
+    public String get(String name) {
+        for (int i = 0; i < city.size(); i++) {
+            if (city.get(i).equals(name)) {
+                return city.get(i);
+            }
+        }
+        return null;
+    }
+    //For-each
+    // public Constable get(String name) {
+    //        for (String s : city) {
+    //            if (s.equals(name)) {
+    //                return s;
+    //            }
+    //        }
+    //        return null;
+    //    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "GraphCities{" +
+                "name='" + name + '\'' +
+                ", city=" + city +
+                '}';
+    }
+}
---
+++ src/Homework14_from_02_10_23/MainGraph.java
@@ -0,0 +1,52 @@
+package Homework14_from_02_10_23;
+
+public class MainGraph {
+    public static void main(String[] args) {
+
+        GraphCities graphCitiesA = new GraphCities("A");
+        GraphCities graphCitiesB = new GraphCities("B");
+        GraphCities graphCitiesV = new GraphCities("V");
+        GraphCities graphCitiesG = new GraphCities("G");
+        GraphCities graphCitiesD = new GraphCities("D");
+        GraphCities graphCitiesE = new GraphCities("E");
+        GraphCities graphCitiesJ = new GraphCities("J");
+        GraphCities graphCitiesZ = new GraphCities("Z");
+        GraphCities graphCitiesI = new GraphCities("I");
+        GraphCities graphCitiesK = new GraphCities("K");
+
+        graphCitiesA.add("B");
+        graphCitiesA.add("V");
+        graphCitiesA.add("G");
+        graphCitiesA.add("D");
+
+        graphCitiesB.add("E");
+        graphCitiesB.add("V");
+
+        graphCitiesV.add("J");
+
+        graphCitiesG.add("V");
+        graphCitiesG.add("Z");
+
+        graphCitiesD.add("G");
+        graphCitiesD.add("I");
+
+        graphCitiesE.add("K");
+
+        graphCitiesJ.add("K");
+        graphCitiesJ.add("Z");
+
+        graphCitiesZ.add("K");
+
+        graphCitiesI.add("K");
+
+        String resultA = graphCitiesA.get("B");
+        System.out.println("Из города А в город: " + resultA);
+
+        String resultB = graphCitiesA.get("V");
+        System.out.println("Из города А в город: " + resultB);
+
+        String resultV = graphCitiesA.get("G");
+        System.out.println("Из города А в город: " + resultV);
+
+    }
+}
