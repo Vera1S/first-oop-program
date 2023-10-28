51177ee9723108a6bde3bd8581c94a267fd801a5
Vera <inginer719@gmail.ru>
2023-10-28 12:29:07 -0400

    Готово

--- src/Homework16_09_10_23/TaskLambdaFunction.java
+++ src/Homework16_09_10_23/TaskLambdaFunction.java
@@ -80,7 +80,7 @@ public class TaskLambdaFunction {
             System.out.println("Введите число: ");
             int num1 = Integer.parseInt(scanner.next());
             for (int i = 0; i < num1; i++) {
-                String result3 = num1 + d;
+                String result3 = String.valueOf(num1 + d);
                 System.out.println(result3); // Я не понимаю, как сделать без цифры
             }
         };
